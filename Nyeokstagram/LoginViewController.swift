1ab0e5ad87e4dc55bcc99b136e38d7a0d369c065
Dongnyeok Shin <dn.shin98@gmail.com>
2022-11-12 16:19:54 +0900

    [fix] .gitignore

---
+++ Nyeokstagram/LoginViewController.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewController.swift
+//  Nyeokstagram
+//
+//  Created by NYEOK on 2022/11/12.
+//
+
+import Foundation
---
+++ Nyeokstagram/Nyeokstagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nyeokstagram
+//
+//  Created by NYEOK on 2022/11/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
