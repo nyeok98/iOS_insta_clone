8d622fe87331ac98127754570f5ba139cdd9034b
Dongnyeok Shin <dn.shin98@gmail.com>
2022-11-12 16:20:48 +0900

    [add] loginView UI setting

--- Nyeokstagram/LoginViewController.swift
+++ Nyeokstagram/LoginViewController.swift
@@ -5,4 +5,27 @@
 //  Created by NYEOK on 2022/11/12.
 //
 
-import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    var email: String?
+    var password: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func emailTextFieldChanged(_ sender: UITextField) {
+        let text = sender.text ?? ""
+        self.email = text
+    }
+    
+    @IBAction func passwordTextFieldChanged(_ sender: UITextField) {
+        let text = sender.text ?? ""
+        self.password = text
+    }
+    
+    @IBAction func loginBtnPushed(_ sender: UIButton) {}
+    
+    @IBAction func registerBtnPushed(_ sender: UIButton) {}
+}
