fb15797daf1792ee17e17db46887128fccfffaae
Dongnyeok Shin <dn.shin98@gmail.com>
2022-11-12 17:15:43 +0900

    [add] connection between Views

--- Nyeokstagram/LoginViewController.swift
+++ Nyeokstagram/LoginViewController.swift
@@ -27,5 +27,16 @@ class LoginViewController: UIViewController {
     
     @IBAction func loginBtnPushed(_ sender: UIButton) {}
     
-    @IBAction func registerBtnPushed(_ sender: UIButton) {}
+    @IBAction func registerBtnPushed(_ sender: UIButton) {
+//         화면전환
+        // 1. 스토리보드를 생성
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        // 2. 뷰컨트롤러 설정
+        let registerViewController = storyboard.instantiateViewController(withIdentifier: "RegisterVC") as! RegisterViewController
+        
+        // 3. 화면전환 메소드를 이용해서 화면을 전환
+//        self.present(registerViewController, animated: true)
+        self.navigationController?.pushViewController(registerViewController, animated: true)
+    }
 }
